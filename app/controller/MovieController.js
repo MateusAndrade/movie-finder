f6d211eb04c89597be7c942754d74974bfce892e
MateusAndrade <mateus.andrade47@outlook.com>
2017-04-10 11:49:43 -0300

    Adicionado metodo para salvar na memória um filme

--- app/controller/MovieController.js
+++ app/controller/MovieController.js
@@ -1,7 +1,19 @@
 app.controller('MovieController', function($scope){
 
     $scope.movie = angular.fromJson(sessionStorage.getItem('movie'));
+    var movies = angular.fromJson(localStorage.getItem('my_movies'));
 
-    console.log($scope.movie);
+    $scope.saveMovie = function(movie){
+        
+        if(localStorage.getItem('my_movies')){
+            movies.push(movie);
+            localStorage.setItem('my_movies',movies);
+            console.log(localStorage.getItem('my_movies'));
+            console.log(movie);
+        } else {
+            localStorage.setItem('my_movies',angular.toJson([movie]));
+        }
+
+    }
 
 });
